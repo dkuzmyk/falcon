475a30260c4ad7a15a977308eb996d72aa2dd83e
dkuzmyk <smapene@yahoo.com>
2021-10-27 02:46:17 -0500

    final

--- client/src/components/Challenge.js
+++ client/src/components/Challenge.js
@@ -1,4 +1,71 @@
+import { useState } from 'react';
+
 export default function Challenge() {
+
+  // to insert the cost value to the total cost
+  const [costText, changeCost] = useState('0');
+  
+  // to insert json data to the table
+  const [data, setItemRow] = useState();
+  
+  // bool value to show the table only when the button is pressed
+  const [showTable, setShowTable] = useState(false);
+  
+  // for input field
+  //const [orderQuantity, setOrderQuantity] = useState('0');
+  
+  var orderItems = []
+  
+  const setOrderQuantity = (value, ID, eState) =>{
+  	console.log("Reorder quantity: "+value+" Reorder ID: "+ID);
+  	//orderItems.push({ID, value});
+  	// update array if ID exists, avoid duplicates
+  	const idx = orderItems.findIndex(a => a.ID === ID);
+  	
+  	if(idx === -1){
+  	  orderItems.push({ID, value});
+  	}
+  	else{
+  	  orderItems[idx].value = value; 
+  	}
+  	
+  	console.log(orderItems);
+  }
+    
+  // low-stock items button handler
+  const orderCost = () => {
+  	console.log('Button order cost working')
+  	
+  	fetch('http://localhost:4567/restock-cost', {
+  	  method: 'POST',
+  	  headers: {"Content-Type" : "application/json"},
+  	  body: JSON.stringify(orderItems)
+  	}).then(response =>{
+  	    //console.log(response)
+  	    return response.json();
+  	}).then(j =>{
+  	    //console.log(j[0]["cost"])
+  	    changeCost(j[0]["cost"])
+  	})
+  	orderItems = []
+  	document.querySelectorAll('input').forEach(input => (input.value = ""));
+  }
+  
+  // determine re-order cost button handler
+  const lowStockButton = () => {
+  	console.log('lowStockButton working')
+  	fetch('http://localhost:4567/low-stock')
+  		.then(res => {
+  			return res.json();  		
+  		})
+  		.then(data => {
+  			console.log(data);
+  			setItemRow(data);
+  			setShowTable(true);
+  		})
+  }
+  
+
   return (
     <>
       <table>
@@ -16,16 +83,45 @@ export default function Challenge() {
           TODO: Create an <ItemRow /> component that's rendered for every inventory item. The component
           will need an input element in the Order Amount column that will take in the order amount and 
           update the application state appropriately.
-          */}
+          */
+          
+          showTable && data.map((row)=> (
+          	<tr>
+          	  <td>{row.ID}</td>
+          	  <td>{row.Name}</td>
+          	  <td>{row.Stock}</td>
+          	  <td>{row.Capacity}</td>
+          	  <td> 
+          	    <input 
+          	      type="text" 
+          	      id="inp"
+          	      placeholder="0"
+          	      //value={orderQuantity}
+          	      onChange={(e)=> setOrderQuantity(e.target.value, row.ID)}
+          	    />
+          	  </td>
+          	</tr>
+          ))
+          
+          }
+          
         </tbody>
       </table>
       {/* TODO: Display total cost returned from the server */}
-      <div>Total Cost: </div>
+      <div>Total Cost: 
+      	<p id="cost">{costText}</p>
+      </div>
       {/* 
       TODO: Add event handlers to these buttons that use the Java API to perform their relative actions.
-      */}
-      <button>Get Low-Stock Items</button>
-      <button>Determine Re-Order Cost</button>
-    </>
+      */
+  
+      }
+      
+      <button onClick={lowStockButton}>Get Low-Stock Items</button>
+      <button onClick={orderCost}>Determine Re-Order Cost</button>      
+
+      
+    </>  
+    
   );
 }
--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,50 @@
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
+import java.util.*;
+
 import static spark.Spark.*;
+
+
 
 public class Main {
 
     public static void main(String[] args) {
+        // 2d listArray to store the xlsx data
+        List<List<String>> inventoryData = new ArrayList<>();
+        List<List<String>>  distributorsData = new ArrayList<>();
+
+        List<List<String>> restock = new ArrayList<>();
+
+        // paths to xlsx
+        String dis_path = "./resources/Distributors.xlsx";
+        String inv_path = "./resources/Inventory.xlsx";
+
+        // get xlsx data into listArray
+        distributorsData = readExcel(dis_path);
+        // clean garbage data in the list
+        distributorsData = cleanDistributors(distributorsData);
+        //printList(distributorsData); // debug
+
+
+        inventoryData = readExcel(inv_path);
+        //printList(inventoryData); // debug
+
+        // find the items that need to be re-ordered
+        restock = findRestockItems(inventoryData);
+        //printList(restock); // debug
+
+        // convert listArray to json
+        JSONArray restockJson = arrayToJson(restock);
+
 
         //This is required to allow GET and POST requests with the header 'content-type'
         options("/*",
@@ -22,13 +64,170 @@ public class Main {
 
         //TODO: Return JSON containing the candies for which the stock is less than 25% of it's capacity
         get("/low-stock", (request, response) -> {
-            return null;
+            System.out.println("Sent Low Stock json");
+            return restockJson;
         });
 
         //TODO: Return JSON containing the total cost of restocking candy
+        List<List<String>> finalDistributorsData = distributorsData;
         post("/restock-cost", (request, response) -> {
-            return null;
+            return managePost(request.body(), finalDistributorsData);
         });
 
+    }
+    // read the excel file and export the listArray of its content
+    public static List<List<String>> readExcel(String path){
+        File distributors = new File(path);
+        // store the 2d array
+        List<List<String>> array = new ArrayList<>();
+        try {
+            // create objects from the two excel files
+            FileInputStream dis = new FileInputStream(distributors);
+
+            // create workbooks to read
+            XSSFWorkbook disWorkbook = new XSSFWorkbook(dis);
+
+            // iterate thro all sheets in the excel file
+            for (int i = 0; i < disWorkbook.getNumberOfSheets(); i++) {
+                XSSFSheet disBook = disWorkbook.getSheetAt(i);
+
+                Iterator<Row> rowIterator = disBook.iterator();
+                // iterate thro all rows in the sheet
+                while (rowIterator.hasNext()) {
+                    List<String> str = new ArrayList<>();
+                    Row row = rowIterator.next();
+                    Iterator<Cell> cellIterator = row.cellIterator();
+                    // iterate thro all cells in each row
+                    while (cellIterator.hasNext()) {
+                        Cell cell = cellIterator.next();
+                        if(!cell.toString().equals("")) {
+                            str.add(cell.toString());// not adding comma
+                        }
+                    }
+                    array.add(str);
+                }
+            }
+
+            disWorkbook.close();
+            //invWorkbook.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        // remove column names from the data or empty strings
+        for (List<String> e : array) {
+            e.removeAll(Arrays.asList("", null));
+        }
+
+        return array;
+    }
+
+    // debug print listarray
+    public static void printList(List<List<String>> l){
+        for (List<String> e : l){
+            for (String s : e){
+                System.out.print(s);
+            }
+            System.out.println();
+        }
+    }
+
+    public static JSONArray managePost(String body, List<List<String>> dis){
+        double totalCost = 0.0;
+
+        // round floating numbers to 2 decimals
+        // using Math.round for simplicity
+
+        // create a json array out of the string received from request
+        JSONArray arr = new JSONArray(body);
+        // creating 2d array with two columns: ID and quantity of items (value)
+        String[][] data = new String[arr.length()][2];
+        // modelling the string into an array
+        for (int i = 0; i < arr.length(); i++) {
+            JSONObject obj = arr.getJSONObject(i);
+            System.out.print("Received Object "+i+" ");
+            System.out.println(obj);
+            data[i][0] = obj.getString("ID");
+            data[i][1] = obj.getString("value");
+
+            //System.out.println(data[i][0] + " " + data[i][1]);
+        }
+
+        // ind the cheapest items by ID
+        for (int i = 0; i < data.length; i++){
+            String itemID = data[i][0];
+            double lowestCostItem = 999999999.99;
+            // the distributors and their items are in distributorsData array
+            // iterate thro the distributors array and find matching ID with the lowest price
+            for (int j = 0; j < dis.size(); j++){
+                if(Objects.equals(dis.get(j).get(1), itemID)){
+                    lowestCostItem = Math.min(lowestCostItem, Double.parseDouble(dis.get(j).get(2)));
+                }
+            }
+            totalCost = totalCost+(lowestCostItem*Integer.parseInt(data[i][1]));
+        }
+        System.out.println("Total cost: "+totalCost);
+        JSONArray a = new JSONArray();
+        JSONObject n = new JSONObject();
+        n.put("cost", String.format("%.2f",totalCost));
+        a.put(n);
+        System.out.println("Sent restock cost");
+        return a;
+    }
+
+    public static List<List<String>> cleanDistributors(List<List<String>> l){
+        List<List<String>> ret = new ArrayList<>();
+        // clean garbage values from the list -> I do it by making sure the value at index 1 is a float
+        for (List<String> e : l){
+            try{
+                if (Float.parseFloat(e.get(1)) > 0.0001){
+                    ret.add(e);
+                }
+            }catch(Exception exc){
+                continue;
+            }
+
+        }
+
+        return ret;
+    }
+
+    public static JSONArray arrayToJson(List<List<String>> l){
+        // column names
+        String[] columns = {"Name", "Stock", "Capacity", "ID"};
+        // crete a jsonarray and populate it with the items to restock
+        JSONArray jsonAr = new JSONArray();
+        for (List<String> e : l){
+            JSONObject obj = new JSONObject();
+            obj.put(columns[0], e.get(0));
+            obj.put(columns[1], e.get(1));
+            obj.put(columns[2], e.get(2));
+            obj.put(columns[3], e.get(3));
+            jsonAr.put(obj);
+        }
+
+        return jsonAr;
+    }
+
+    public static List<List<String>> findRestockItems(List<List<String>> l){
+        // simply remove all rows that have Stock/Capacity < 0.25 and return the 2d arraylist
+        l.remove(0); // remove the name of columns at index 0
+
+        List<List<String>> ret = new ArrayList<>();
+
+        //System.out.println((l.get(0).get(1))+(l.get(0).get(1)));
+        // calculate the quantity of items that is below 25% and add them to the list to return
+        for (List<String> row : l){
+            if (Float.parseFloat(row.get(1))/Float.parseFloat(row.get(2)) < 0.25){
+                System.out.println("Adding "+row.get(0) + " to the refill list");
+                ret.add(row);
+            }
+        }
+
+        return ret;
+    }
+
+    public static JSONArray calculateRestockCost(){
+
+        return null;
     }
 }
